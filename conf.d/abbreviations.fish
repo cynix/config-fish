6d4936dbd57bfda8967426c5465af89f62d8444f
cynix <cynix@cynix.org>
2020-01-26 15:55:27 +1100

    Add abbreviations for ssh without host key checking

--- conf.d/abbreviations.fish
+++ conf.d/abbreviations.fish
@@ -1,3 +1,5 @@
 abbr l ls
 abbr ll 'ls -lAh'
 abbr x exit
+abbr nssh 'ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
+abbr nscp 'scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
