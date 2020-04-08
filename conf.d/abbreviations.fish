6e7037776bc9bce5c9c7738afe43e39d43487057
cynix <cynix@cynix.org>
2020-04-08 21:42:51 +1000

    Update abbreviations

--- conf.d/abbreviations.fish
+++ conf.d/abbreviations.fish
@@ -1,5 +1,5 @@
 abbr l ls
 abbr ll 'ls -lah'
 abbr x exit
-abbr nssh 'ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
-abbr nscp 'scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
+abbr nssh 'ssh -o UserKnownHostsFile=/dev/null'
+abbr nscp 'scp -o UserKnownHostsFile=/dev/null'
