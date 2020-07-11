f8338ccc1e4b3a75a53ca088eb862c1a2647f4c6
cynix <cynix@cynix.org>
2020-07-11 20:02:27 +1000

    Add fisher

---
+++ conf.d/fisher.fish
@@ -0,0 +1,15 @@
+set -g fisher_path ~/.config/fish/fisher
+
+set fish_function_path $fish_function_path[1] $fisher_path/functions $fish_function_path[2..-1]
+set fish_complete_path $fish_complete_path[1] $fisher_path/completions $fish_complete_path[2..-1]
+
+if not functions -q fisher
+	set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+	mkdir -p $XDG_CONFIG_HOME/fish/fisher/functions
+	curl -sLo $XDG_CONFIG_HOME/fish/fisher/functions/fisher.fish https://raw.githubusercontent.com/jorgebucaran/fisher/master/fisher.fish
+	fish -c fisher
+end
+
+for file in $fisher_path/conf.d/*.fish
+	builtin source $file 2> /dev/null
+end
