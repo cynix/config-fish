c9848461111fdb8d7c59f74f9e6bd99bab98c63a
cynix <cynix@cynix.org>
2024-01-13 11:15:23 +1100

    Add tailscale alias

---
+++ functions/tailscale.fish
@@ -0,0 +1,5 @@
+if test -x /Applications/Tailscale.app/Contents/MacOS/Tailscale
+  function tailscale --wraps=/Applications/Tailscale.app/Contents/MacOS/Tailscale --description 'alias tailscale /Applications/Tailscale.app/Contents/MacOS/Tailscale'
+    /Applications/Tailscale.app/Contents/MacOS/Tailscale $argv
+  end
+end
