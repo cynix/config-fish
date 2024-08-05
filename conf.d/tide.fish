bf8e82212c15a45e25a1bdac2bd77a86cf5829af
cynix <cynix@cynix.org>
2024-08-05 23:30:29 +1000

    Update tide config

--- conf.d/tide.fish
+++ conf.d/tide.fish
@@ -1,6 +1,6 @@
 if status is-interactive && type -q tide
   if ! set -q tide__configured
-    tide configure --auto --style=Lean --prompt_colors='True color' --show_time='24-hour format' --lean_prompt_height='Two lines' --prompt_connection=Solid --prompt_connection_andor_frame_color=Darkest --prompt_spacing=Sparse --icons='Few icons' --transient=No
+    tide configure --auto --style=Lean --prompt_colors='True color' --show_time='24-hour format' --lean_prompt_height='Two lines' --prompt_connection=Dotted --prompt_connection_andor_frame_color=Darkest --prompt_spacing=Sparse --icons='Few icons' --transient=No
     set -U tide__configured true
   end
   set -U tide_left_prompt_items context pwd git newline character
