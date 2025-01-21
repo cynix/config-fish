a988e43fc1a514bcc9395168fd8b2dcb35ebdb25
cynix <cynix@cynix.org>
2025-01-21 12:51:07 +0000

    Update zellij pane title

--- conf.d/zellij.fish
+++ conf.d/zellij.fish
@@ -1,9 +1,26 @@
-if command -sq zellij && not functions -q __auto_zellij_tab_name
-  function __auto_zellij_tab_name --on-variable PWD --description "Update zellij tab name on directory change"
-    if set -q ZELLIJ
-      nohup zellij action rename-tab (prompt_pwd) >/dev/null 2>&1
-    end
-  end
+if set -q ZELLIJ
+  # https://github.com/fish-shell/fish-shell/blob/945a53557038062eeb0cae551f8014cef5f3310f/share/functions/fish_title.fish
+  function fish_title
+    # If we're connected via ssh, we print the hostname.
+    set -l ssh
+    set -q SSH_TTY; and set ssh "["(prompt_hostname | string sub -l 10 | string collect)"]"
 
-  __auto_zellij_tab_name
+    # An override for the current command is passed as the first parameter.
+    # This is used by `fg` to show the true process name, among others.
+    set -l command
+    if set -q argv[1]
+      set command $argv[1]
+    else
+      # Don't print "fish" because it's redundant
+      set command (status current-command)
+      if test "$command" = "fish"
+        set command
+      end
+    end
+
+    echo -- $ssh (string sub -l 20 -- $command) (prompt_pwd -d 1 -D 1)
+
+    # Make Zellij generate a PaneUpdate event _after_ title has been set
+    sh -c 'zellij action rename-pane "" &'
+  end
 end
