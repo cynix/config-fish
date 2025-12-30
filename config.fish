148b39b0550d79c4a14d984b1a918a32897e9ce2
cynix <cynix@cynix.org>
2025-12-30 11:48:50 +1100

    Set default key bindings

--- config.fish
+++ config.fish
@@ -1,3 +1,5 @@
+set --global fish_key_bindings fish_default_key_bindings
+
 fish_add_path $HOME/go/bin $HOME/.cargo/bin $HOME/.local/share/gem/ruby/bin
 # Move these to the front
 fish_add_path -m $HOME/.local/bin
